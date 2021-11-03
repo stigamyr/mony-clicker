d0444d63987645e913e0003b03f41fc1dfed5f58
stig <dtiamu17@vgs.nfk.no>
2021-11-03 15:30:06 +0100

    shop
    jeg har laget en sho som fungere hvor du må ha 10 lemo og når du kjøpper så trekker den i far  og at prisen øker

--- dollar.js
+++ dollar.js
@@ -1,21 +1,34 @@
 var dollars = 0;
 var lemo = 0;
-var shop = 10;
+var pizza = 0;
+var lemon_price = 10;
+
+var intervaldId = window.setInterval(function(){
+    // all kode her kjøres 10 ganger i sekundet
+    document.getElementById("dollar_earnd").innerHTML=dollars;
+    
+}, 100);
+
+
 
 var intervaldId = window.setInterval(function(){
     // all kode her kjøres 10 ganger i sekundet
     document.getElementById("dollar_earnd").innerHTML=dollars;
     add_lemon();
+    add_pizza();
 }, 1000);
 
 
 
 
+
 function add_lemon() {
-    dollars +=  10 * lemo;
+    
+     
+      dollars +=  10 * lemo;
+
+
 }
-
-
 
 
 
@@ -26,15 +39,30 @@ dollars++;
  
 
 
-function click_lemon(){
+function buy_lemon(){
+    if(dollars >= lemon_price ){
+        lemo += 1;
+        dollars -= lemon_price;
+        lemon_price *= 3,3;
+        console.log(dollars);
+    }
     
-    lemo+= 1;
-    console.log(dollars);
 }
 function click_news(){
     dollars+=50;
     console.log(dollars);
 }
+
+
+function click_pizza(){
+    pizza += 1
+}
+
+  function add_pizza(){
+      dollars += 10 * pizza
+  }
+
+
 
 
 
