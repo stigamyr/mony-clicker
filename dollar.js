c5b1b6e16dc850f54cdfb79aad5caaff3e1a10f3
stig <92983848+stigamyr@users.noreply.github.com>
2021-11-05 13:40:05 +0100

    fredags time
    jeg har laget en h1 rG SOM VISER PRICEN FOR  AVIS OG DEN HVISER NÅR PRISEN FÅR OP

--- dollar.js
+++ dollar.js
@@ -1,7 +1,14 @@
 var dollars = 0;
 var lemo = 0;
 var pizza = 0;
+var avis  = 0;
 var lemon_price = 10;
+var avis_price = 10;
+
+
+
+document.getElementById("avis_price").innerHTML =  avis_price + "$";
+
 
 var intervaldId = window.setInterval(function(){
     // all kode her kjøres 10 ganger i sekundet
@@ -16,8 +23,8 @@ var intervaldId = window.setInterval(function(){
     document.getElementById("dollar_earnd").innerHTML=dollars;
     add_lemon();
     add_pizza();
+    add_news();
 }, 1000);
-
 
 
 
@@ -25,7 +32,7 @@ var intervaldId = window.setInterval(function(){
 function add_lemon() {
     
      
-      dollars +=  10 * lemo;
+      dollars +=  1* lemo;
 
 
 }
@@ -43,20 +50,27 @@ function buy_lemon(){
     if(dollars >= lemon_price ){
         lemo += 1;
         dollars -= lemon_price;
-        lemon_price *= 3,3;
+        lemon_price *= 5,5;
         console.log(dollars);
     }
     
 }
-function click_news(){
-    dollars+=50;
-    console.log(dollars);
+function add_news(){
+   dollars += 1* avis;
 }
 
 
-function click_pizza(){
-    pizza += 1
+function buy_avis(){
+
+    if(dollars >= avis_price){
+        avis += 1;
+        dollars -= avis_price;
+        avis_price *= 10.10;
+        document.getElementById("avis_price").innerHTML =  avis_price + "$";
+    }
 }
+
+
 
   function add_pizza(){
       dollars += 10 * pizza
